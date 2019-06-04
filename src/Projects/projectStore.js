d52d94ac4a8ff8eb002c80bd0e5a2df77664e949
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-06-03 22:04:46 -0400

    sizing

--- src/Projects/projectStore.js
+++ src/Projects/projectStore.js
@@ -6,8 +6,8 @@ export default {
 			"description": "ONGOING: RecipeBox is an evolving full-stack product. Users can submit recipes to a shared recipe box, and search for recipes by title. Please see the ReadMe for upcoming plans for this project. Updates are made frequently.",
 			"technologies": "React, Node.js/Express.js",
 			"live": "https://recipebox.victorialgodfrey.now.sh/",
-			"repo": "https://github.com/inertiawhoosh/my-recipe-box"
-		
+			"repo": "https://github.com/inertiawhoosh/my-recipe-box",
+
 		},
 		{
 			"title": "MAWNJ Splash Page",
