cd2ccf4654c2a20ea1e169552c3bccc8de49a61a
Tori <44040482+inertiawhoosh@users.noreply.github.com>
2021-05-24 18:12:54 -0600

    Update AboutBlurb.js

--- src/AboutBlurb/AboutBlurb.js
+++ src/AboutBlurb/AboutBlurb.js
@@ -11,7 +11,7 @@ class AboutBlurb extends React.Component {
 			I'm Tori, a web developer with a background in Customer Success.
 			</p>
 			<p id = "bio">
-			My passion for front-end development started in middle school when I built and sold custom profiles for an online game. My interest expanded as I put my skills to use making Live Journal layouts and customizing my MySpace page. In hindsight, that is a very early 2000 sentence. Though, my path back to web development is unconventional, and has taken me all over the world working in fields ranging from education to policy, I am happy to be doing what I truly love. When I'm not pushing to github, I like to work on my waltz jump (ice skating), go to theme parks, and try my hand at home brewing.
+			My passion for front-end development started in middle school when I built and sold custom profiles for online games. My interest expanded as I put my skills to use making Live Journal layouts and customizing my MySpace page. In hindsight, that is a very early 2000 sentence. Though, my path back to web development is unconventional, and has taken me all over the world working in fields ranging from education to policy, I am happy to be doing what I truly love. When I'm not pushing to github, I like to work on my write, cross stitch, hike, and horeseback ride.
 			</p>
 		</section>
 
